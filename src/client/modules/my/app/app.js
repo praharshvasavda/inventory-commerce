45474c56ae1fed58cc35b0cf69950184030e78fb
praharsh vasavda <praharsh.d.vasavda@pwc.com>
2021-06-08 09:19:25 +0530

    Constant data added

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -14,7 +14,22 @@ export default class App extends LightningElement {
         .then((resp) => resp.json())
         .then((data)=> {
             console.log(data);
-            this.listings = data.records;
+            //this.listings = data.records;
+            this.listings = [
+                    {
+                        brand : 'Volkswagen',
+                        model : 'Golf 1.6 TDI',
+                        registrationDate : 1614417465600,
+                        power : 75,
+                        color : 'White',
+                        sku : '434122505',
+                        img : 'https://i.imgur.com/HarnXBx.jpg',
+                        stockType : 'Used',
+                        price : 12500,
+                        mileage : 266917
+
+                    }
+                ]
         })
         .catch((error)=> {
         console.log(error);
