93e59933064a605485801f272a95908f617d0418
praharsh vasavda <praharsh.d.vasavda@pwc.com>
2021-06-08 12:02:42 +0530

    updated

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -10,30 +10,30 @@ export default class App extends LightningElement {
     }
 
     fetchListings(){
-        // fetch('/api/listing')
-        // .then((resp) => resp.json())
-        // .then((data)=> {
-        //     console.log(data);
-            //this.listings = data.records;
-            this.listings = [
-                    {
-                        brand : 'Volkswagen',
-                        model : 'Golf 1.6 TDI',
-                        registrationDate : 1614417465600,
-                        power : 75,
-                        color : 'White',
-                        sku : '434122505',
-                        img : 'https://i.imgur.com/HarnXBx.jpg',
-                        stockType : 'Used',
-                        price : 12500,
-                        mileage : 266917
+        fetch('/api/listing')
+        .then((resp) => resp.json())
+        .then((data)=> {
+            console.log(data);
+            this.listings = data.records;
+            // this.listings = [
+            //         {
+            //             brand : 'Volkswagen',
+            //             model : 'Golf 1.6 TDI',
+            //             registrationDate : 1614417465600,
+            //             power : 75,
+            //             color : 'White',
+            //             sku : '434122505',
+            //             img : 'https://i.imgur.com/HarnXBx.jpg',
+            //             stockType : 'Used',
+            //             price : 12500,
+            //             mileage : 266917
 
-                    }
-                ]
-        // })
-        // .catch((error)=> {
-        // console.log(error);
-        // });
+            //         }
+            //     ]
+        })
+        .catch((error)=> {
+        console.log(error);
+        });
     }
 
 
